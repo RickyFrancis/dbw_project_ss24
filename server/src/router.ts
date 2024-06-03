ab51ab92516da581a108960df0ceb647ffc1b064
Ricky Francis Rozario <rickyfrancisrozario@gmail.com>
2024-06-03 21:38:16 +0200

    Initial Commit

---
+++ server/src/router.ts
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+
+const router = Router();
+
+/**
+ * Entity
+ */
+router.get('/entity', () => {});
+router.get('/entity/:id', () => {});
+router.post('/entity', () => {});
+router.put('/entity/:id', () => {});
+router.delete('/entity/:id', () => {});
+
+/**
+ * Schule
+ */
+router.get('/schule', () => {});
+router.get('/schule/:id', () => {});
+router.post('/schule', () => {});
+router.put('/schule/:id', () => {});
+router.delete('/schule/:id', () => {});
+
+export default router;
